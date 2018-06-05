d79e7b6d86d9baab8e7ef03719bcd18eb97ca015
dangdang6666 <dangdang6666@outlook.com>
2018-06-05 09:39:38 +0800

    Initial commit

---
+++ Charpter3/makeTextFile.py
@@ -0,0 +1,26 @@
+import os
+ls = os.linesep
+
+"""get file name"""
+while True:
+    fname = input("Enter fname: ")
+    if os.path.exists(fname):
+        print("error: {} is already exists".format(fname))
+    else:
+        break
+
+"""get file content"""
+al = []
+print("\nEnter lines ('.') by itself to quit.\r")
+
+while True:
+    entry = input('> ')
+    if entry == '.':
+        break
+    else:
+        al.append(entry)
+
+"""white lines to file with proper line-ending"""
+fobj = open(fname, 'w')
+fobj.writelines(["{}{}".format(x, ls) for x in al])
+fobj.close()
---
+++ Charpter3/test.py
@@ -0,0 +1,4 @@
+import os
+ls = os.linesep
+
+print("a"+ ls + "b")
