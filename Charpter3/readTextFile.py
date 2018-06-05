bf3712ce119682910f62458a2389090d107c3a15
dangdang6666 <dangdang6666@outlook.com>
2018-06-05 11:43:59 +0800

    读取文本文件和异常简介初始版

--- Charpter3/readTextFile.py
+++ Charpter3/readTextFile.py
@@ -24,5 +24,5 @@ IOError    I/O操作引发的异常，如打开文件出错等
 KeyError  使用字典中不存在的关键字引发的异常
 NameError 使用不存在的变量名引发的异常
 TabError 语句块缩进不正确引发的异常
-ValueError 搜索列表中不存在的值引发的异常
+ValueError 搜索列表中不存在的值引发的-异常
 ZeroDivisionError   除数为零引发的异常"""
